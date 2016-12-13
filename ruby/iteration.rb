77869be0e09fc549beaea1ebca3c44e62b353205
Joseph Huang <huangx5j@gmail.com>
2016-12-13 15:44:44 -0800

    Added method that iterates through the items with .each and .map! for array & hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,28 @@
+#5.3 Pairing
+#Joseph Huang
+#Brian Bernheim
+
+#Method that takes a block
 def status
 	puts "Before passing a block"
 	2.times {yield}
 end
 
 status {puts "After passing a block"}
+
+#method that iterates through the items with .each and .map! for array & hash
+arr1=["name1","name2","name3"]
+hash1={name1: "bob", name2:"ben", name3:"billy"}
+
+arr1.each {|name| puts name}
+arr1.map! do |name|
+  puts "Before modification: #{name}"
+  mod=name+" Miller"
+  puts "After modification: #{mod}"
+end
+
+hash1.each {|name| puts name}
+hash1.map do |k, v|
+  puts "Before modification: #{v}"
+  puts "After modification: #{"%#{v}%"}"
+end
