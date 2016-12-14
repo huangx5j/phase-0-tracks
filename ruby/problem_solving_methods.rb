0789ac7a02e0a7464da73c5cd420ced1fc7176fb
Joseph Huang <huangx5j@gmail.com>
2016-12-14 14:26:26 -0800

    Added method that takes a number of Fibonacci terms to generate and returns an array of the terms

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,4 +1,9 @@
 
+# 5.6 Pairing
+# Joseph Huang
+# Brendan Schwarz
+
+#Method that takes an array of integers and an integer to search for. The method should return the index of the item, or nil if the integer is not present in the array.
 def search(arr, num)
   i = 0
   while i < arr.length 
@@ -15,3 +20,21 @@ def search(arr, num)
 end
 
 puts search([42, 89, 23, 0], 1) 
+
+#Method to your file that takes a number of Fibonacci terms to generate and returns an array of the terms
+def fibonacci(n) 
+  arr = [0]
+  n.times do |i|
+    if i==0
+      arr[i] = 0
+    elsif i==1
+      arr[i] = 1
+    else
+      arr[i] = arr[i-1] + arr[i-2]
+    end  
+  end
+
+  return arr
+end
+
+print fibonacci(6) #=>[0,1,1,2,3,5]
