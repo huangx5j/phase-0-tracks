3545066d0bbaffba82f0a5e8a21cdf9d5057d7e5
Joseph Huang <huangx5j@gmail.com>
2016-12-18 23:12:07 -0800

    Created nested hash for a coding school

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+#Design and build a nested data structure to represent a real-world construct
+
+#Coding school with list of teachers, locations, students, and levels
+code_school = {name: "Devbootcamp",
+	classroom: { 
+	  	location: ["Chicago", "Houston", "San_Francisco"],
+	  	teachers: {
+	  		name1: "Denny", 
+	  		name2: "Andrew", 
+	  		name3: "Jenny", 
+	},
+	students: {
+	  level: ["beginner", "intermediate", "advanced"]
+	}
+	}
+}
+
