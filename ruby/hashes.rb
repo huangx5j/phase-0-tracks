eb6837f74e9ee6b16478a102afa648e6c99941b3
Joseph Huang <huangx5j@gmail.com>
2016-12-18 20:29:09 -0800

    Added methods to allow user to update a key and print the lastest hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -47,3 +47,39 @@ for i in 0...form.length
 end
 
 
+puts "","Is there an answer you would like to revise? (yes/no)"
+answer1 = nil;
+answer2 = nil;
+
+while answer1 = gets.chomp.downcase;
+	case answer1 
+		when "yes"
+			puts "","What portion of the form would you like to revise? Select 'none' to exit."
+			while answer2 = gets.chomp.downcase;
+				if form.key?(answer2) then
+					puts "","What is your new answer regarding #{answer2}?"
+					form[answer2] = gets.chomp;
+					break;
+				elsif answer2 == "none" then
+					puts "Thank you for answering"
+					  break;
+				else
+					puts "This is not on the form."
+				end
+			end
+			break;
+		when "no"
+			puts "","Thank you for answering the questions."
+			break;
+		when "none"
+			puts "","No change. We hope to see you again"
+			break;
+		else
+			puts "Error, please type 'yes' or 'no'. Type 'none' to exit."
+	end
+end
+
+puts "","Your answers below:"
+for i in 0...form.length
+  puts "- #{form.keys[i]}: #{form.values[i]}"
+end
