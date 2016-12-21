cc5bbc9411364803b43e50dfd3c8caa93f7be71a
Joseph Huang <huangx5j@gmail.com>
2016-12-21 14:23:21 -0800

    6.2 Release 0 methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,36 @@
 class Puppy
-
+  
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(n)
+    n.times { puts "Woof!" }
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def sit
+    puts "*sits*"
+  end
+  
+  def dog_years(human_years)
+    human_years * 7
 
+  end
+  
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 end
 
+#driver
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over
+fido.dog_years(4)
+fido.sit
