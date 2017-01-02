e75ee7b2cb938860d3f7f509a2d127ae9dbdf1c7
Joseph Huang <huangx5j@gmail.com>
2017-01-01 23:17:52 -0800

    Created shout module with two methods

---
+++ ruby/shout.rb
@@ -0,0 +1,16 @@
+# Write a simple module
+module Shout
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+  	words + "!!!" + " :D"
+  end
+
+end
+
+# puts Shout.yell_angrily("I hate deubgging!")
+# puts Shout.yell_happily("I fixed the bug!")
+
