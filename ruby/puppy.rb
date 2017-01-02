a762a14b138ca0214755efbb1d7f017d9330fbc6
Joseph Huang <huangx5j@gmail.com>
2017-01-01 17:35:00 -0800

    Created Puppy class with driver code

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+class Puppy
+
+	duchess = Puppy.new
+	fido = Puppy.new
+	spot = Puppy.new
+
+	#driver code
+	# spot.class #=>Puppy
+	# puts duchess == fido #=>false
+	# fido.instance_of?(Array) #=>false
+	# fido.instance_of?(Puppy) #=>true
+	# spot.play_dead #=>undefined
+
+end
+
