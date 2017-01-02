92392591b66381f0528223d1e4fd9ea302dee4be
Joseph Huang <huangx5j@gmail.com>
2017-01-01 20:25:55 -0800

    Initialize santa instance

---
+++ ruby/santa.rb
@@ -0,0 +1,11 @@
+def speak
+	puts "Ho, ho, ho! Haaaappy holidays!"
+end
+
+def eat_milk_and_cookies(cookie)
+	puts "That was a good #{cookie}!"
+end
+
+def initialize
+	puts "Initializing Santa instance ..."
+end
