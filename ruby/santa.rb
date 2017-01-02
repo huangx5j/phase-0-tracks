3322c904952bf4526b39001bb0b3b37f2636e730
Joseph Huang <huangx5j@gmail.com>
2017-01-01 20:57:44 -0800

    Added more initializations using arrays

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,38 @@
-def speak
-	puts "Ho, ho, ho! Haaaappy holidays!"
+class Santa
+
+	def speak
+		puts "#{@gender} Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+	def initialize(gender, ethnicity)
+		# puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+
 end
 
-def eat_milk_and_cookies(cookie)
-	puts "That was a good #{cookie}!"
+# driver code
+
+# santa= Santa.new
+# santa.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+genders = ["female", "male", "bisexual", "asexual"]
+ethnicities = ["Asian", "White", "Black", "European", "Other"]
+
+genders.length.times do |i|
+	santas << Santa.new(genders[i], ethnicities[i])
 end
 
-def initialize
-	puts "Initializing Santa instance ..."
+puts "Testing each Santa instance"
+santas.each do |speaker|
+  speaker.speak
 end
