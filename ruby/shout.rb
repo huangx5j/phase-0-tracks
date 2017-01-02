1a57e8eecd4a3c35702b1203b9539ed71bc1c822
Joseph Huang <huangx5j@gmail.com>
2017-01-01 23:27:13 -0800

    Added two classes to include Shout module and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,43 @@
 # Write a simple module
-module Shout
+# module Shout
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-  	words + "!!!" + " :D"
-  end
+#   def self.yelling_happily(words)
+#   	words + "!!!" + " :D"
+#   end
 
-end
+# end
 
 # puts Shout.yell_angrily("I hate deubgging!")
 # puts Shout.yell_happily("I fixed the bug!")
 
+# Convert a Standalone Module to a Mixin
+
+module Shout
+  def yell_angrily(words)
+  	words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+  	words + "!!!" + " :D"
+  end
+end
+
+class Dog
+	include Shout
+end
+
+class Cat
+	include Shout
+end
+
+dog = Dog.new
+puts dog.yell_angrily("Bark!")
+puts dog.yelling_happily("Woof!")
+
+cat = Cat.new
+puts cat.yell_angrily("Hiss!")
+puts cat.yelling_happily("Purr!")
