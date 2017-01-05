38e94ac623cef8103fb0ff90de146d9a66bf933b
Joseph Huang <huangx5j@gmail.com>
2017-01-04 17:15:37 -0800

    Created car constructor and call it multiple times

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,22 @@ for (var k=0; k < names.length; k++)
 
 }
 console.log(horse)
+
+console.log("----------------------")
+
+function Car(model,year,color)
+{
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.yell = function() { console.log("what a nice car!!")}
+	this.shout = function() { console.log("what a piece of junk!!")}
+
+}
+var new_car = new Car("Toyota",2011, "blue")
+console.log(new_car);
+new_car.yell();
+
+var old_car = new Car("Mazda",1990, "sliver")
+console.log(old_car);
+new_car.shout();  
