b7017b8f1b118236215d7211f7cdaee50c34fabf
Joseph Huang <huangx5j@gmail.com>
2017-01-04 17:13:54 -0800

    Created javascript arrays and loop for object

---
+++ js/data_structures.js
@@ -0,0 +1,14 @@
+var colors = ["red", "blue", "green", "yellow"]
+var names = ["Bill", "Ed", "Mack", "Ned"]
+
+colors.push("pink");
+names.push("Chad");
+
+var horse = {} ;
+
+for (var k=0; k < names.length; k++)
+{
+	horse[names[k]] = colors[k]
+
+}
+console.log(horse)
