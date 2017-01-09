4bd4c9b2c8740a42f0071a129a686f2dfb2f95a8
Joseph Huang <huangx5j@gmail.com>
2017-01-09 01:54:36 -0800

    Added psuedcode for compare method

--- js/algos.js
+++ js/algos.js
@@ -1,10 +1,10 @@
 // Release 0
 // A function that takes an array of words 
 // and returns the longest word.
-// Have a temp variable to hold longest word and loop
+// Input: array of words
+// Steps: Have a temp variable to hold longest word and loop
 // through elements of the array to check if current element
 // is longer and replace it in the temp variable if so. 
-// Input: array of words
 // Output: longest word in the array
 
 var words = ["long phrase","longest phrase","longer phrase"]
@@ -20,6 +20,15 @@ function longestWord(arr) {
 	} 
 	return word;
 }
+
+// Release 1
+// A function takes two objects and checks to see 
+// if the objects share at least one key-value pair.
+// Input : 2 objects
+// Steps: Create 2 empty arrays to hold keys and values.
+// Loop arrays to compare and if an element is equal then
+// return true. Else return false.
+// Output: true or false
 
 // Driver code
 console.log(longestWord(words));
