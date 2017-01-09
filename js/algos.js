f2a2eac5f175328d53af6368b5d19164753ea97c
Joseph Huang <huangx5j@gmail.com>
2017-01-09 01:41:29 -0800

    Added psuedocode for function longestWord

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+// Release 0
+// A function that takes an array of words 
+// and returns the longest word.
+// Have a temp variable to hold longest word and loop
+// through elements of the array to check if current element
+// is longer and replace it in the temp variable if so. 
+// Input: array of words
+// Output: longest word in the array
+
+
+
+
+
