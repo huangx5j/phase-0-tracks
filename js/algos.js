336eeb33a872b07f1b91290efda74e6880523a4e
Joseph Huang <huangx5j@gmail.com>
2017-01-09 02:04:19 -0800

    Added compare function with driver code

--- js/algos.js
+++ js/algos.js
@@ -30,5 +30,27 @@ function longestWord(arr) {
 // return true. Else return false.
 // Output: true or false
 
+function compare(obj1, obj2) {
+	var arr1 = [];
+	var arr2 = [];
+	for (var key1 in obj1) {
+		arr1.push(key1, obj1[key1]);
+	}
+	for (var key2 in obj2) {
+		arr2.push(key2, obj2[key2]);
+	}
+	for (var i = 0; i < arr1.length; i++) {
+		if (arr1[i] == arr2[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
 // Driver code
 console.log(longestWord(words));
+
+console.log(compare({name: "Steven", age: 54},{name: "Janet", age: 64}));
+console.log(compare({car: "Toyota", year: 2011},{name: "Chen", age: 30}));
