7a6f4b70a7192063ddf8629a92edb4b4b77fe734
Joseph Huang <huangx5j@gmail.com>
2017-01-09 01:27:54 -0800

    Created reverse method with pseudocode

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// Takes a string as input
+// Splits input, reverses it, and store in array
+// Print out reversed string
+
+
+function reverse (str) {
+	return str.split('').reverse().join('')
+}
+
+reverse("joseph")
